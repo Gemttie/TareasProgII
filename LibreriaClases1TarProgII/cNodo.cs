5a35c611f191e4803d7d6ac455880df9d77ad2c8
Alumno_DAII <newprovidentia@gmail.com>
2024-10-14 12:08:24 -0500

    Subida tarea nodos

---
+++ LibreriaClases1TarProgII/cNodo.cs
@@ -0,0 +1,51 @@
+﻿namespace LibreriaClases1TarProgII
+{
+    public class cNodo
+    {
+        //atributos
+        public object aElemento;
+        public cNodo aSgteNodo;
+
+        //metodos
+        //constructores
+        public cNodo()
+        {
+            aElemento = null;
+            aSgteNodo = null;
+        }
+
+        public cNodo(object pEelemento, cNodo pSgteNodo)
+        {
+            aElemento = pEelemento;
+            aSgteNodo = pSgteNodo;
+        }
+
+        //set y get
+        public object GetElemento()
+        {
+            return aElemento;
+        }
+
+        
+        public void SetElemento(object value)
+        {
+            aElemento = value;
+        }
+
+        
+        public cNodo GetSgteNodo()
+        {
+            return aSgteNodo;
+        }
+
+        
+        public void SetSgteNodo(cNodo value)
+        {
+            aSgteNodo = value;
+        }
+        //otros metodos
+
+
+
+    }
+}
